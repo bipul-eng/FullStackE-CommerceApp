c673aafeb7418f2aa3a515c3b4fcad469c96bdd7
bipulkr-java <singharjundev2@gmail.com>
2025-01-30 10:58:33 +0530

    Applied Validations using JSON Web Tokens And Spring Security

---
+++ ecommerce-1/src/main/java/com/bk/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.bk.config;
+
+public class JwtConstant {
+
+	public static final String SECRET_KEY="abcdefghijklmnopjghgjhjfjfgfgdfgsdfjgqrstuvwxyz";
+	public static final String JWT_HEADER="Authorization";
+}
--- ecommerce-1/src/main/java/com/bk/modal/Cart.java
+++ ecommerce-1/src/main/java/com/bk/modal/Cart.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -12,6 +13,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 
+@Entity
 public class Cart {
 	
 	@Id
